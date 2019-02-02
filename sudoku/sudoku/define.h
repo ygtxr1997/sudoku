d4ba9c5d7818f9030a9f1128354f738c1fa3cc7a
ygtxr1997 <gavingreenson@icloud.com>
2019-02-02 16:57:53 +0800

    Add sudoku producer, but it takes too long timegit add *.cpp!

--- sudoku/sudoku/define.h
+++ sudoku/sudoku/define.h
@@ -6,6 +6,7 @@
 #include <stack>
 #include <set>
 #include <unordered_set>
+#include <map>
 
 #define TRACE_CMH_2(fmt,...) \
 	printf("%s(%d)-<%s>: "##fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
@@ -35,6 +36,29 @@ typedef struct Sudoku
 		_uCol(SD_COL)
 	{}
 
+	void print() const
+	{
+		for (UINT8 i = 0; i < _uRow; ++i)
+		{
+			if (!(i % 3))
+			{
+				for (UINT8 k = 0; k < _uCol; ++k)
+					printf("---");
+				printf("\n");
+			}
+			for (UINT8 j = 0; j < _uCol; ++j)
+			{
+				if (!(j % 3)) printf("| ");
+				printf("%u ", _ppLayout[i][j] + 1);
+			}
+			printf("|\n");
+		}
+		for (UINT8 k = 0; k < _uCol; ++k)
+			printf("---");
+		printf("\n");
+	}
+
+	// 二维矩阵中值为-1代表空，0~8代表1~9
 	UINT8	_ppLayout[SD_ROW][SD_COL];
 	UINT8	_uRow;
 	UINT8	_uCol;
--- sudoku/sudoku/main.cpp
+++ sudoku/sudoku/main.cpp
@@ -1,4 +1,4 @@
-#include "./model/YGSudokuJudge.h"
+#include "./model/YGSudokuProducer.h"
 #include <iostream>
 
 UINT8 GMATRIX[6][7] =
@@ -12,37 +12,37 @@ UINT8 GMATRIX[6][7] =
 };
 
 // 唯一解的数独
-UINT8 GSUDOKU[9][9] =
-{
-{ 8,0,0,0,0,0,0,0,0 },
-{ 0,0,3,6,0,0,0,0,0 },
-{ 0,7,0,0,9,0,2,0,0 },
-{ 0,5,0,0,0,7,0,0,0 },
-{ 0,0,0,0,4,5,7,0,0 },
-{ 0,0,0,1,0,0,0,3,0 },
-{ 0,0,1,0,0,0,0,6,8 },
-{ 0,0,8,5,0,0,0,1,0 },
-{ 0,9,0,0,0,0,4,0,0 }
-};
-
-// 有多个解的数独
 //UINT8 GSUDOKU[9][9] =
 //{
-//{ 1,7,5,8,3,9,4,2,6 },
-//{ 0,3,0,2,7,4,9,1,5 },
-//{ 4,2,9,6,5,1,3,7,8 },
-//{ 0,1,0,3,9,5,7,4,2 },
-//{ 5,4,7,1,6,2,8,3,9 },
-//{ 2,9,3,4,8,7,6,5,1 },
-//{ 7,5,4,9,2,6,1,8,3 },
-//{ 9,8,1,5,4,3,2,6,7 },
-//{ 3,6,2,7,1,8,5,9,4 }
+//{ 8,0,0,0,0,0,0,0,0 },
+//{ 0,0,3,6,0,0,0,0,0 },
+//{ 0,7,0,0,9,0,2,0,0 },
+//{ 0,5,0,0,0,7,0,0,0 },
+//{ 0,0,0,0,4,5,7,0,0 },
+//{ 0,0,0,1,0,0,0,3,0 },
+//{ 0,0,1,0,0,0,0,6,8 },
+//{ 0,0,8,5,0,0,0,1,0 },
+//{ 0,9,0,0,0,0,4,0,0 }
 //};
+
+// 可能有多个解的数独
+UINT8 GSUDOKU[9][9] =
+{
+{ 1,7,5,8,3,9,4,2,6 },
+{ 0,3,6,2,7,4,9,1,5 },
+{ 4,2,9,6,5,1,3,7,8 },
+{ 0,1,0,3,9,5,7,4,2 },
+{ 5,4,7,1,6,2,8,3,9 },
+{ 2,9,3,4,8,7,6,5,1 },
+{ 7,5,4,9,2,6,1,8,3 },
+{ 9,8,1,5,4,3,2,6,7 },
+{ 3,6,2,7,1,8,5,9,4 }
+};
 
 int main()
 {
 	// 验证DLX
-	Matrix mMatrix(6, std::vector<bool>(7));
+	/*Matrix mMatrix(6, std::vector<bool>(7));
 	for (UINT32 row = 0; row < 6; ++row)
 	{
 		for (UINT32 col = 0; col < 7; ++col)
@@ -52,21 +52,33 @@ int main()
 	}
 
 	YGDancingLinks YGDLX(6, 7, mMatrix);
-	YGDLX.startDance();
+	YGDLX.startDance();*/
 
-	// 验证Sudoku
+	// 验证Sudoku计算者
+	printf("--------验证Sudoku计算者--------:\n");
 	Sudoku sudoku;
 	for (UINT8 i = 0; i < 9; ++i)
 	{
 		for (UINT8 j = 0; j < 9; ++j)
 		{
-			sudoku._ppLayout[i][j] = GSUDOKU[i][j];
+			sudoku._ppLayout[i][j] = GSUDOKU[i][j] - 1;
 		}
 	}
 	sudoku._uRow = 9;
 	sudoku._uCol = 9;
 	YGSudokuJudge ygSudokuJudge(sudoku);
 	ygSudokuJudge.printAnswer();
+
+	// 验证Sudoku生产者
+	printf("--------验证Sudoku生产者--------:\n");
+	YGSudokuProducer ygSudokuProducer;
+	Sudoku sdQuestion, sdAnswer;
+	ygSudokuProducer.getQuestion(sdQuestion);
+	ygSudokuProducer.getAnswer(sdAnswer);
+	printf("Question:\n");
+	sdQuestion.print();
+	printf("Answer:\n");
+	sdAnswer.print();
 
 	int a;
 	std::cin >> a;
